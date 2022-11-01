438901d14a052d03adac6b9e058413c2db62ea89
Emmanuel Enya <emmanuelenya@meltwater.org>
2022-11-01 07:44:32 +0000

    creates a product data class

---
+++ product.py
@@ -0,0 +1,17 @@
+import uuid
+
+class Product:
+    _id = ""
+    name = ""
+    description = ""
+    quantity = 0
+    price = 0.00
+    discount = 0.00
+
+
+    def __init__(self, name, quantity, price) -> None:
+        self._id = uuid.uuid4()
+        self.name = name
+        self.quantity = quantity
+        self.price = price
+
